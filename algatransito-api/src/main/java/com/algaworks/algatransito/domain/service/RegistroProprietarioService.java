f633eeb1236d1d8b8b94802368fd8b1e1833df1c
Hugo Alexandro <hugo.a.dev@gmail.com>
2023-12-19 20:30:14 -0300

    test

--- algatransito-api/src/main/java/com/algaworks/algatransito/domain/service/RegistroProprietarioService.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/domain/service/RegistroProprietarioService.java
@@ -16,7 +16,7 @@ public class RegistroProprietarioService {
     @Transactional
     public Proprietario salvar(Proprietario proprietario) {
         boolean emailEmUso = proprietarioRepository.findByEmail(proprietario.getEmail())
-                .filter(p -> !p.equals(proprietario))
+                .filter(p -> !p.equals(proprietario) )
                 .isPresent();
 
         if (emailEmUso) {
