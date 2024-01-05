7acf09df8d0b79c5aa94542fc4f7c165f9918452
Hugo Alexandro <hugo.a.dev@gmail.com>
2024-01-05 06:07:52 -0300

    5.13. Criando um Representation Model para entrada de dados

---
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/model/input/ProprietarioIdInput.java
@@ -0,0 +1,12 @@
+package com.algaworks.algatransito.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProprietarioIdInput {
+
+    private Long id;
+    private String nome;
+}
--- algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,16 +1,10 @@
 package com.algaworks.algatransito.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Getter
@@ -24,29 +18,20 @@ public class Veiculo {
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotNull
-    @Valid
     @ManyToOne
     private Proprietario proprietario;
 
-    @JsonProperty(access =  JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusVeiculo status;
 
-    @JsonProperty(access =  JsonProperty.Access.READ_ONLY)
     private OffsetDateTime dataCadastro;
 
-    @JsonProperty(access =  JsonProperty.Access.READ_ONLY)
     private OffsetDateTime dataApreensao;
 
-    @NotBlank
     private String marca;
 
-    @NotBlank
     private String modelo;
 
-    @NotBlank
-    @Pattern(regexp = "[A-Z]{3}[0-9][0-9A-Z][0-9]{2}")
     private String placa;
 
 }
