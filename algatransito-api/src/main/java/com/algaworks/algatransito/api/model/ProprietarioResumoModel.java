a50c99d9d6653c05c8f5349a7b684769f6d82738
Hugo Alexandro <hugo.a.dev@gmail.com>
2024-01-05 05:30:07 -0300

    5.12. Compondo objetos no Representation Model

---
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/model/ProprietarioResumoModel.java
@@ -0,0 +1,12 @@
+package com.algaworks.algatransito.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProprietarioResumoModel {
+
+    private Long id;
+    private String nome;
+}
